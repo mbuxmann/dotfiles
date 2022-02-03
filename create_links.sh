b510604613bc5dcb56a1471a4ed994426cb38eff
Martin <mbuxmann@outlook.com>
2022-02-03 20:56:28 +0200

    first commit

---
+++ create_links.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+
+ln -s ~/.doom.d doom
+ln -s ~/.Xdefaults .Xdefault
+ln -s ~/.xinitrc .xinitrc
+ln -s ~/.Xresources .Xresources
+ln -s ~/.zshrc .zshrc
+
+ln -s ~/.config/bspwm bspwm
+ln -s ~/.config/coc coc
+ln -s ~/.config/eww eww
+ln -s ~/.config/nvim nvim
+ln -s ~/.config/picom picom
+ln -s ~/.config/polybar polybar
+ln -s ~/.config/rofi rofi
+
